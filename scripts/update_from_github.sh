047090553c488fe804197292c7c8046b2338dd4d
Daniel Zbinden <88660575+ZbindenDaniel@users.noreply.github.com>
2024-07-09 11:20:29 +0200

    add cron job setup

--- scripts/update_from_github.sh
+++ scripts/update_from_github.sh
@@ -12,9 +12,4 @@ cd $LOCAL_DIR
 # Pull the latest changes from the repository
 git pull $REPO_URL
 
-# Restart the services
-sudo systemctl restart PlanthouseHub.service
-sudo systemctl restart relay_control.service
-sudo systemctl restart ota_update.service
-
 echo "Update completed and services restarted"
